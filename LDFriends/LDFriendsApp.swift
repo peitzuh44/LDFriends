cd2198e19d64c21f28b8ac0f25fb2b9fdd40dff5
Pei Huang <pei_huang@tomofun.com>
2025-03-27 17:51:40 +0800

    Initial Commit

---
+++ LDFriends/LDFriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LDFriendsApp.swift
+//  LDFriends
+//
+//  Created by RD-Pei_Hung on 2025/3/27.
+//
+
+import SwiftUI
+
+@main
+struct LDFriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
